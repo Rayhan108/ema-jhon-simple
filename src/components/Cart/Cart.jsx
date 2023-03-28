c6a10b0f8208b604bda416d5deacbf96b3dbc4e8
Rayhan Shorker <raihanshorker56@gmail.com>
2023-03-29 00:51:49 +0600

    show data fron cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import "./Cart.css"
-const Cart = ({cart}) => {
-    // console.log(cart)
-let totalPrice =0;
-let totalShiping =0;
-    for(const product of cart){
-        // console.log(product.price)
-        totalPrice+=product.price
-        totalShiping +=product.shipping;
+import React from "react";
+import "./Cart.css";
+const Cart = ({ cart }) => {
+  // console.log(cart)
+  let totalPrice = 0;
+  let totalShiping = 0;
+  let quantity=0;
+  for (const product of cart) {
+    if(product.quantity===0){
+        product.quantity=1;
     }
-    const tax = totalPrice*7/100;
-    const grandTotal = totalPrice+totalShiping+tax;
-    return (
-        <div className='cart'>
-                <h4>Order summary</h4>
-              <p>selected items:{cart.length}</p>
-              <p>Total Price:${totalPrice}</p>
-              <p>Shiping:${totalShiping}</p>
-              <p>Tax:${tax.toFixed(2)}</p>
-              <h4>Grand Total:${grandTotal.toFixed(2)}</h4>
-        </div>
-    );
+    // console.log(product.price)
+    totalPrice += product.price*product.quantity;
+    totalShiping += product.shipping;
+    quantity+=product.quantity;
+  }
+  const tax = (totalPrice * 7) / 100;
+  const grandTotal = totalPrice + totalShiping + tax;
+  return (
+    <div className="cart">
+      <h4>Order summary</h4>
+      <p>selected items:{quantity}</p>
+      <p>Total Price:${totalPrice}</p>
+      <p>Shiping:${totalShiping}</p>
+      <p>Tax:${tax.toFixed(2)}</p>
+      <h4>Grand Total:${grandTotal.toFixed(2)}</h4>
+    </div>
+  );
 };
 
 export default Cart;
