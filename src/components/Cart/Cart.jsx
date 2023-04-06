ab9aef8b064d6eb14539a886bb6d59c1f79a425c
Rayhan Shorker <raihanshorker56@gmail.com>
2023-04-06 17:37:32 +0600

    implement clear cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Cart.css";
-const Cart = ({ cart }) => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+const Cart = ({ cart,handleClearCart }) => {
   // console.log(cart)
   let totalPrice = 0;
   let totalShiping = 0;
@@ -24,6 +26,7 @@ const Cart = ({ cart }) => {
       <p>Shiping:${totalShiping}</p>
       <p>Tax:${tax.toFixed(2)}</p>
       <h4>Grand Total:${grandTotal.toFixed(2)}</h4>
+      <button onClick={handleClearCart} className="btn-clear-cart">Clear Cart  <FontAwesomeIcon  icon={faTrashAlt}/></button>
     </div>
   );
 };
--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -3,7 +3,7 @@ import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import "./Order.css"
-import { removeFromDb } from '../../utilities/fakedb';
+import { deleteShoppingCart, removeFromDb } from '../../utilities/fakedb';
 
 const Order = () => {
     const savedCart =useLoaderData()
@@ -14,6 +14,11 @@ const Order = () => {
 const remaining = cart.filter(product=>product.id !== id);
 setCart(remaining)
 removeFromDb(id)
+
+    }
+    const handleClearCart =()=>{
+        setCart([])
+        deleteShoppingCart()
 
     }
     // console.log(cart)
@@ -28,7 +33,7 @@ removeFromDb(id)
               </div>
            
             <div className="cart-container">
-                        <Cart cart={cart}></Cart>
+                        <Cart handleClearCart={handleClearCart} cart={cart}></Cart>
                     </div>
          
         </div>
